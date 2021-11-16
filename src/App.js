1819aaaf8d2883ad78acfb765b0e541d5325a7ba
Pratik Nugurwar <pratiknugurwar1@gmail.com>
2021-11-16 14:53:22 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import List from "./List";
+import data from "./data";
+const App = () => {
+  const [people, setPeople]=useState(data)
 
-function App() {
+  const onChangeHandler =()=>
+  {
+    setPeople([])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people}/>
+        <button onClick={onChangeHandler}>Clear All</button>
+      </section>
+    </main>
   );
-}
+};
 
 export default App;
